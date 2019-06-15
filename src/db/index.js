5fed58c7492a1ae6a9fdd9ec3e96249114837a85
Руслан Муфтиев <muftik@yandex-team.ru>
2019-06-15 18:00:39 +0300

    Simple json DB

---
+++ src/db/index.js
@@ -0,0 +1,5 @@
+let providers = require('./providers.json');
+let consumers = require('./consumers.json');
+
+export const getProviders = () => providers;
+export const getConsumers = () => consumers;
