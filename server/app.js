0fd5b6da50cfb2a3cdd9b0c4db3ae6b6b32444fd
Alexander Vlasov <stuartrevol@gmail.com>
2019-06-15 15:12:03 +0300

    Express app

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from "cors";
+import { serverPort, apiPrefix } from "./config";
+const app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(cors()); // корс для хедов
+
+const server = app.listen(serverPort, () => { // слушатель на serverPort
+    console.log("Server run on the " + apiPrefix)
+});
