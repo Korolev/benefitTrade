ae3ed7a7956452e4acf1390efd8555c102f0c7e3
Руслан Муфтиев <muftik@yandex-team.ru>
2019-06-15 15:04:10 +0300

    Home and Profile pages + routes

---
+++ src/components/routes/home-page.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+class HomePage extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <Fragment>
+        <h2>HomePage</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default HomePage
