ade82270954b930db2731c3c1bb180129ca85e4a
mk-sfdev <mk-sfdev@mk-sfdevs-MacBook-Pro.local>
2019-06-15 22:08:59 +0300

    add grid component + star rating

--- src/components/routes/home-page.js
+++ src/components/routes/home-page.js
@@ -1,10 +1,38 @@
 import React, { Component, Fragment } from 'react'
 import Menu, { MenuItem } from '../menu'
+import Grid from '../grid'
+import { getConsumers, getProviders } from '../../db/index'
+import StarRating from '../starrating'
 
 class HomePage extends Component {
   static propTypes = {}
 
   render() {
+    const traders = getProviders()
+
+    const dataMap = {
+      name: 'Название компании',
+      phone: 'Котактный телефон',
+      type: 'Статус поставщика',
+      categories: 'Категории поставщика',
+      rating: 'Рейтинг'
+    }
+
+    const columnSequences = Object.keys(dataMap)
+
+    const columnComponents = {
+      rating: (rating) => <StarRating starsSelected={rating} />
+    }
+
+    const sortByKey = (ascdesc) => (key) => (a, b) => {
+      if (ascdesc === 'asc') {
+        return a[key] < b[key] ? 1 : -1
+      } else {
+        return a[key] < b[key] ? -1 : 1
+      }
+    }
+
+    const items = traders.sort(sortByKey('asc')('rating'))
     return (
       <Fragment>
         <Menu>
@@ -13,6 +41,12 @@ class HomePage extends Component {
           <MenuItem link="/login">LoginPage</MenuItem>
         </Menu>
         <h2>HomePage</h2>
+        <Grid
+          items={items}
+          dataMap={dataMap}
+          columnSequences={columnSequences}
+          columnComponents={columnComponents}
+        />
       </Fragment>
     )
   }
