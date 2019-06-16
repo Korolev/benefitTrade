9fe445c0d11ab5cb03224a7105ced96de7f13a09
Alexander Vlasov <stuartrevol@gmail.com>
2019-06-16 10:27:37 +0300

    Inputs

--- src/components/routes/profile-page.js
+++ src/components/routes/profile-page.js
@@ -4,11 +4,36 @@ import history from '../../helpers/history'
 import Menu, { MenuItem } from '../menu'
 import UserBlock from '../user-block'
 import { userSelector, userLoggedInSelector } from '../../selectors'
-import InputForm from '../input/InputForm'
 import './profile-page.css'
+import Icon from '../icon'
 
 class ProfilePage extends Component {
-  static propTypes = {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        name: 'ИП Мирохина А. Н.',
+        inn: '12345667890',
+        phones: ['+7(123)456-78-90', '+7(123)456-71-71'],
+        addresses: [
+          'Республика Крым, г. Симферополь, ул. Вернадского, д. 4',
+          'Республика Крым, г. Симферополь, ул. Кирова, д. 3'
+        ],
+        specif: {
+          Бакалея: ['Каши', 'Супы']
+        }
+      }
+    }
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+
+  handleEdit = (prop) => (event) => {
+    this.setState({
+      user: {
+        [prop]: event.target.value
+      }
+    })
+  }
 
   componentDidMount() {
     this.checkLogin()
@@ -19,6 +44,16 @@ class ProfilePage extends Component {
   }
 
   render() {
+    const { user, readonly } = this.state
+
+    const pensilIcon = (
+      <div className="edit-icon-container">
+        <div className="edit-icon">
+          <Icon type="pencil" />
+        </div>
+      </div>
+    )
+
     return (
       <Fragment>
         <Menu>
@@ -51,20 +86,79 @@ class ProfilePage extends Component {
             <div className="row custom-row">
               <div className="col-sm-3 custom-col">
                 <div className="col-content">
-                  <UserBlock
-                    size="l"
-                    user={this.props.user}
-                  />
+                  <UserBlock size="l" user={this.props.user} />
                 </div>
               </div>
               <div className="col-sm custom-col">
-                <div className="col-content">
-                  <InputForm
-                    title={'Название юр. лица'}
-                    data={'ИП Мирохина А. Н.'}
-                    readonly={true}
-                  />
-                </div>
+                <form className="input-form">
+                  <div className="col-content">
+                    <div className="input-container">
+                      <label className="input-container__title">
+                        Название юр. лица:
+                      </label>
+                      <input
+                        className="input"
+                        type="text"
+                        title={'Название юр. лица'}
+                        value={user.name}
+                        onChange={this.handleEdit('name')}
+                      />
+                    </div>
+                    {pensilIcon}
+                  </div>
+
+                  <div className="col-content">
+                    <div className="input-container">
+                      <label className="input-container__title">ИНН:</label>
+                      <input
+                        className="input"
+                        type="text"
+                        title={'ИНН'}
+                        value={user.inn}
+                        onChange={this.handleEdit('inn')}
+                      />
+                    </div>
+                    {pensilIcon}
+                  </div>
+
+                  <div className="col-content">
+                    <div className="input-container">
+                      <label className="input-container__title">
+                        Контактный телефон:
+                      </label>
+                      {user.phones.map((item, index) => (
+                        <input
+                          className="input"
+                          key={index + item}
+                          type="text"
+                          title={'Контактный телефон'}
+                          value={item}
+                          //onChange={this.handleEdit("phones")}
+                        />
+                      ))}
+                    </div>
+                    {pensilIcon}
+                  </div>
+
+                  <div className="col-content">
+                    <div className="input-container">
+                      <label className="input-container__title">
+                        Адреса торговых точек:
+                      </label>
+                      {user.addresses.map((item, index) => (
+                        <input
+                          className="input"
+                          key={index + item}
+                          type="text"
+                          title={'Адрес'}
+                          value={item}
+                          //onChange={this.handleEdit("addresses")}
+                        />
+                      ))}
+                    </div>
+                    {pensilIcon}
+                  </div>
+                </form>
               </div>
               <div className="col-sm custom-col">
                 <div className="col-content">
